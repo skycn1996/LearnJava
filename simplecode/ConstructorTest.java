44fb20875f5041679bc1c2c05fa92dc785b93112
wangxl <wangxl@zhenglaikeji.com>
2018-05-17 23:47:48 +0800

    add some java source code

---
+++ simplecode/ConstructorTest.java
@@ -0,0 +1,15 @@
+public class ConstructorTest{
+	public String name;
+	public int count;
+
+	public ConstructorTest(String name, int count){
+		this.name=name;
+		this.count=count;
+	}
+
+	public static void main(String[] args){
+		ConstructorTest tc = new ConstructorTest("疯狂Java讲义",90000);
+		System.out.println(tc.name);
+		System.out.println(tc.count);
+	}
+}
