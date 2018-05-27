41aceb7263b6ae1ab295ab3db5c59056f0ef92e0
wangxl <wangxl@zhenglaikeji.com>
2018-05-27 17:42:45 +0800

    add rotatepic.java

--- netbeansprj/simpprj/simpprj/src/Welcome.java
+++ netbeansprj/simpprj/simpprj/src/Welcome.java
@@ -8,8 +8,19 @@
  *
  * @author Administrator
  */
+
+import  java.util.Scanner;
 public class Welcome {
     public static void main(String[] args){
         System.out.println("Welcome to Java!");
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a number  for radius:"); 
+        double radius = input.nextDouble();
+        
+        double area = radius * radius * 3.14159;
+        
+        System.out.println("The area for the circle  of radius "+ radius + " is " + area);
     }
 }
