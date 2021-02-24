ce9479e114f63e9f947d6dd921882cbf2dbb991e
skycn1996 <skycn1996@126.com>
2021-02-24 14:24:07 +0800

    测试IoC

---
+++ springAll/demo1/src/main/java/entity/Classes.java
@@ -0,0 +1,39 @@
+package entity;
+
+import java.util.List;
+
+public class Classes {
+    private int id;
+    private String name;
+    private List<Student> students;
+
+    @Override
+    public String toString() {
+        return "Classes{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", students=" + students.toString() +
+                '}';
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
