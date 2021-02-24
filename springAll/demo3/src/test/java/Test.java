2661fa134295b433ebea78299f38a0ec54073b82
skycn1996 <skycn1996@126.com>
2021-02-24 19:17:21 +0800

    demo3

---
+++ springAll/demo3/src/test/java/Test.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import entity.Car;
+import entity.Person;
+
+
+public class Test {
+	public static void main(String[] args) throws SQLException {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+//		Car car = (Car) applicationContext.getBean("car1");
+//		System.out.println(car);
+		
+//		Car car2 = (Car) applicationContext.getBean("car2");
+//		System.out.println(car2);
+		
+		Person person = (Person) applicationContext.getBean("person");
+		System.out.println(person);
+	}
+}
