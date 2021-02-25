ba9d84ad5069c85ff920dc886aaddce8e4026d89
skycn1996 <skycn1996@126.com>
2021-02-25 11:12:05 +0800

    new project for spring mvc xml

---
+++ springAll/springxml01/src/main/java/com/wxl/services/HelloService.java
@@ -0,0 +1,10 @@
+package com.wxl.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String sayHello() {
+        return "南无阿弥陀佛";
+    }
+}
